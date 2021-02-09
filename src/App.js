cab9ba474edf797f1de202c8bb6ca10c69535c19
hex <786019030@qq.com>
2021-02-09 16:18:21 +0800

    初始化项目,并添加homepage页面 menu-item directory组件 更新 menu-item 组件 withRouter

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import { Switch, Route } from "react-router-dom";
+import HomePage from "./pages/homepage/homepage.component";
+
+import "./App.css";
+
+const HatsPage = () => (
+  <div>
+    <h1>Hats Pages</h1>
+  </div>
+);
+
+function App() {
+  return (
+    <div>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop/hats" component={HatsPage} />
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
